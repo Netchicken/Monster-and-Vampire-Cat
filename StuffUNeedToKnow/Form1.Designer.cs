ce293647183433f8c06433275ae07e29401f58fa
Gary Dix <intelproof@gmail.com>
2024-07-15 11:56:39 +1200

    update

--- StuffUNeedToKnow/Form1.Designer.cs
+++ StuffUNeedToKnow/Form1.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // btnRun
             // 
-            this.btnRun.Location = new System.Drawing.Point(377, 122);
+            this.btnRun.Location = new System.Drawing.Point(11, 235);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(2);
             this.btnRun.Name = "btnRun";
-            this.btnRun.Size = new System.Drawing.Size(228, 52);
+            this.btnRun.Size = new System.Drawing.Size(152, 34);
             this.btnRun.TabIndex = 0;
             this.btnRun.Text = "Show Image";
             this.btnRun.UseVisualStyleBackColor = true;
@@ -47,30 +48,32 @@
             // lbxOutput
             // 
             this.lbxOutput.FormattingEnabled = true;
-            this.lbxOutput.ItemHeight = 20;
-            this.lbxOutput.Location = new System.Drawing.Point(105, 94);
+            this.lbxOutput.Location = new System.Drawing.Point(11, 58);
+            this.lbxOutput.Margin = new System.Windows.Forms.Padding(2);
             this.lbxOutput.Name = "lbxOutput";
-            this.lbxOutput.Size = new System.Drawing.Size(213, 264);
+            this.lbxOutput.Size = new System.Drawing.Size(143, 173);
             this.lbxOutput.TabIndex = 1;
             // 
             // pbxImage
             // 
             this.pbxImage.Image = global::StuffUNeedToKnow.Resource1.cat;
-            this.pbxImage.Location = new System.Drawing.Point(380, 180);
+            this.pbxImage.Location = new System.Drawing.Point(189, 26);
+            this.pbxImage.Margin = new System.Windows.Forms.Padding(2);
             this.pbxImage.Name = "pbxImage";
-            this.pbxImage.Size = new System.Drawing.Size(225, 225);
+            this.pbxImage.Size = new System.Drawing.Size(333, 267);
             this.pbxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbxImage.TabIndex = 2;
             this.pbxImage.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(527, 324);
             this.Controls.Add(this.pbxImage);
             this.Controls.Add(this.lbxOutput);
             this.Controls.Add(this.btnRun);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pbxImage)).EndInit();
